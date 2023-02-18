c63b5dc15c13c0a330c07e1313fb506e43ad2806
Hegodi <diegonher@gmail.com>
2023-02-18 11:33:58 +0100

    Trained models for GatherFruits

--- GatherFruits/main_training.py
+++ GatherFruits/main_training.py
@@ -5,14 +5,13 @@ sys.path.append("..\\")
 from delega import TrainingSettings, Training
 
 settings = TrainingSettings()
-settings.result_name = "train02"
-settings.save_model_period = 100
-settings.save_log_period = 20
-settings.n_games = 1000
-settings.epsilon_dec = 1.0e-2
+settings.result_name = "train_64"
+settings.save_model_period = 50
+settings.save_log_period = 25 
+settings.n_games = 500
+settings.epsilon_dec = 2.0e-3
 settings.epsilon_min = 1e-4
-settings.max_duration_game = 10.0
-settings.save_period = 100
+settings.max_duration_game = 15.0
 settings.update_nn_period = 10
 
 if __name__ == '__main__':
